4734aedb2d1579458cac52e8e41d8359a31fadf5
Serhii Diachenko <s.diachenko87@gmail.com>
2018-07-18 14:07:49 -0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div :key="name" v-for="name of names">
+      <MyComponent :name='name'/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import MyComponent from "./components/myComponent.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    MyComponent
+  },
+  data() {
+    return {
+      names: ["bob", "bil", "sam", "lil"]
+    };
   }
 };
 </script>
@@ -24,5 +34,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: aquamarine;
 }
 </style>
---
+++ src/components/myComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cont">
+    <h1>My name is MY COMPONENT {{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: {
+    name: String
+  }
+};
+</script>
+
+<style scoped>
+.cont {
+  border: black solid;
+  background-color: beige;
+}
+</style>
